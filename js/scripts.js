ebe18c66acb50eb4f74ec62f8eff31be1150b78c
HannahChege <chegehannah45@gmail.com>
2018-07-20 17:42:40 +0300

    added top nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 //Business logic
 function Player(name,roundtotal,totalscore) {  // player Constructor declared
-  this.name=name; //Properties Declared
+ this.name=name; //Properties Declared
   this.roundtotal=roundtotal;
   this.totalscore=totalscore;
 }
+var throwdice = function () {
+  return Math.floor(6*Math.random())+1;
+}
 //user interface Logic
 $(document).ready(function(){
-  $("#result").click(function(event){
-  event.preventDefault();
-  var player1 = $("#Player1Name"). val();
-  var player2 = $("#Player2Name"). val();
+  $("form#Player").submit(function(event){
+    event.preventDefault();
+    var player1 = $("input#Player1Name").val();
+    var player2 = $("input#Player2Name").val();
 
-  $("#Player1Name").text(player1)
-  $("#Player2Name").text(player2)
-  })
-})
+    $("h1#Player1Name").text(player1);
+    $("h1#Player2Name").text(player2);
+    console.log(player1 + " " + player2);
+
+  });
+  $()
+});
